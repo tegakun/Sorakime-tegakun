c134bb454b4c3f626a04e37d0315cacd9ea6d4db
Sorakime <69241694+Sorakime@users.noreply.github.com>
2024-04-05 09:48:10 +0900

    [FIX}

--- script/common.js
+++ script/common.js
@@ -2,9 +2,14 @@ function menu() {
   document.getElementById('menu').classList.toggle('open');
 }
 window.onload = () => {
-  let sheets = document.styleSheets;
+  let element = document.createElement('style');
+  element.innerText = `
+    body::before {left: 0vw;}
+  `;
+  document.body.insertBefore(element);
+  /*let sheets = document.styleSheets;
   sheets[sheets.length - 1].insertRule(
     'body::before {left: 0vw;}',
     sheets[sheets.length - 1].cssRules.length
-  );
+  );*/
 };
