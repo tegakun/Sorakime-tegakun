6d84b53927e94ba7b2e7b51d03b913a0427413c8
Sorakime <69241694+Sorakime@users.noreply.github.com>
2024-04-05 23:37:15 +0900

    [changed]パスの修正をなかったことにして、[add]contact

--- script/common.js
+++ script/common.js
@@ -7,9 +7,4 @@ window.onload = () => {
     body::before {left: 0vw;}
   `;
   document.body.appendChild(element);
-  /*let sheets = document.styleSheets;
-  sheets[sheets.length - 1].insertRule(
-    'body::before {left: 0vw;}',
-    sheets[sheets.length - 1].cssRules.length
-  );*/
 };
